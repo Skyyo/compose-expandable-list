99da2f294fd881e68033fedf9f96ba602236267d
Sean Proctor <sproctor@gmail.com>
2021-08-06 14:49:25 -0400

    simplified and removed constants

--- app/src/main/java/com/skyyo/expandablelist/cards/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.skyyo.expandablelist.cards
-
-const val EXPAND_ANIMATION_DURATION = 300
-const val COLLAPSE_ANIMATION_DURATION = 300
-const val FADE_IN_ANIMATION_DURATION = 350
-const val FADE_OUT_ANIMATION_DURATION = 300
--- app/src/main/java/com/skyyo/expandablelist/cards/ExpandableCard.kt
+++ app/src/main/java/com/skyyo/expandablelist/cards/ExpandableCard.kt
@@ -1,19 +1,16 @@
 package com.skyyo.expandablelist.cards
 
-import android.annotation.SuppressLint
 import androidx.compose.animation.*
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.Indication
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.contentColorFor
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
@@ -24,56 +21,47 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
-import com.skyyo.expandablelist.R
 
-@SuppressLint("UnusedTransitionTargetStateParameter")
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun ExpandableCard(
     modifier: Modifier = Modifier,
     title: @Composable () -> Unit,
     toggle: () -> Unit,
     expanded: Boolean,
+    contentColor: Color? = null,
     expandedBackgroundColor: Color = MaterialTheme.colors.background,
     collapsedBackgroundColor: Color = MaterialTheme.colors.background,
     expandedElevation: Dp = 1.dp,
     collapsedElevation: Dp = 1.dp,
     shape: Shape = MaterialTheme.shapes.medium,
+    animationDurationMillis: Int = AnimationConstants.DefaultDurationMillis,
     content: @Composable () -> Unit
 ) {
-    val transitionState = remember {
-        MutableTransitionState(expanded).apply {
-            targetState = !expanded
-        }
-    }
+    val transitionState = remember { MutableTransitionState(expanded) }
+    transitionState.targetState = expanded
     val transition = updateTransition(transitionState, label = "transition")
     val cardBgColor by transition.animateColor({
-        tween(durationMillis = EXPAND_ANIMATION_DURATION)
+        tween(durationMillis = animationDurationMillis)
     }, label = "bgColorTransition") {
-        if (expanded) expandedBackgroundColor else collapsedBackgroundColor
+        if (it) expandedBackgroundColor else collapsedBackgroundColor
     }
     val cardElevation by transition.animateDp({
-        tween(durationMillis = EXPAND_ANIMATION_DURATION)
+        tween(durationMillis = animationDurationMillis)
     }, label = "elevationTransition") {
-        if (expanded) expandedElevation else collapsedElevation
+        if (it) expandedElevation else collapsedElevation
     }
     val arrowRotationDegree by transition.animateFloat({
-        tween(durationMillis = EXPAND_ANIMATION_DURATION)
+        tween(durationMillis = animationDurationMillis)
     }, label = "rotationDegreeTransition") {
-        if (expanded) 0f else 180f
+        if (it) 0f else -180f
     }
 
     Card(
         backgroundColor = cardBgColor,
-        contentColor = Color(
-            ContextCompat.getColor(
-                LocalContext.current,
-                R.color.colorDayNightPurple
-            )
-        ),
+        contentColor = contentColor ?: contentColorFor(backgroundColor = cardBgColor),
         elevation = cardElevation,
         shape = shape,
         modifier = modifier,
@@ -90,7 +78,13 @@ fun ExpandableCard(
                 CardArrow(degrees = arrowRotationDegree)
                 title()
             }
-            ExpandableContent(visible = expanded, content = content)
+            AnimatedVisibility(
+                visible = expanded,
+                enter = expandVertically() + fadeIn(),
+                exit = shrinkVertically() + fadeOut(),
+            ) {
+                content()
+            }
         }
     }
 }
@@ -107,43 +101,5 @@ fun CardArrow(
                 .rotate(degrees)
                 .align(Alignment.Center),
         )
-    }
-}
-
-@OptIn(ExperimentalAnimationApi::class)
-@Composable
-fun ExpandableContent(
-    visible: Boolean = true,
-    content: @Composable () -> Unit,
-) {
-    val enterFadeIn = remember {
-        fadeIn(
-            animationSpec = TweenSpec(
-                durationMillis = FADE_IN_ANIMATION_DURATION,
-                easing = FastOutLinearInEasing
-            )
-        )
-    }
-    val enterExpand = remember {
-        expandVertically(animationSpec = tween(EXPAND_ANIMATION_DURATION))
-    }
-    val exitFadeOut = remember {
-        fadeOut(
-            animationSpec = TweenSpec(
-                durationMillis = FADE_OUT_ANIMATION_DURATION,
-                easing = LinearOutSlowInEasing
-            )
-        )
-    }
-    val exitCollapse = remember {
-        shrinkVertically(animationSpec = tween(COLLAPSE_ANIMATION_DURATION))
-    }
-    AnimatedVisibility(
-        modifier = Modifier.padding(8.dp),
-        visible = visible,
-        enter = enterExpand + enterFadeIn,
-        exit = exitCollapse + exitFadeOut,
-    ) {
-        content()
     }
 }
